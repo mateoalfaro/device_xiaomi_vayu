6b015a757e3caa40121088de4799e1b02ce80e83
mateoalfaro <73367365+mateoalfaro@users.noreply.github.com>
2023-12-23 20:37:44 -0600

    Bringup PixelBuilds Unity!
    Adapt basic changes for PixelBuilds

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -11,8 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common AOSP configurations
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit common PixelBuilds configurations
+$(call inherit-product, vendor/pb/config/common_full_phone.mk)
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_AOSP_RECOVERY := true
