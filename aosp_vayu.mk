f96444986b9018448846bea963800af5c243a457
mateoalfaro <73367365+mateoalfaro@users.noreply.github.com>
2024-08-28 07:31:44 -0600

    flags: remove aperture

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -15,6 +15,7 @@ $(call inherit-product, device/xiaomi/vayu/device.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
+PRODUCT_NO_CAMERA := true
 
 
 PRODUCT_NAME := aosp_vayu
